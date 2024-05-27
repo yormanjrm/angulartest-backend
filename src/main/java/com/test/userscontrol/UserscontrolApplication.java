28b97cc8df670df7acf17fb317ae264417ac6a8b
Yorman Jesus Rojas Martinez <yorman.rojas@neoris.com>
2024-05-27 15:46:59 -0600

    init project

---
+++ src/main/java/com/test/userscontrol/UserscontrolApplication.java
@@ -0,0 +1,13 @@
+package com.test.userscontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserscontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserscontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/userscontrol/UserscontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.userscontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserscontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
