b169f6bb7bbc047fd125f978cafff10345c5b09f
Yorman Jesus Rojas Martinez <yorman.rojas@neoris.com>
2024-06-06 18:36:54 -0600

    fix image

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -12,4 +12,4 @@ INSERT INTO users (name, email, password, role, image)
 VALUES ('John Doe', 'john.doe@example.com', '$2a$10$/iOt2Bx.vd4PHhWWJttE3.mNlX/jriVf4dASqOwbijXR.0goXmyRe', 'ADMIN', 'http://localhost:8080/images/default.png');
 
 INSERT INTO users (name, email, password, role, image)
-VALUES ('Jane Doe', 'jane.doe@example.com', '$2a$10$/iOt2Bx.vd4PHhWWJttE3.mNlX/jriVf4dASqOwbijXR.0goXmyRe', 'RECEP', 'http://localhost:8080/images/default.jpg');
+VALUES ('Jane Doe', 'jane.doe@example.com', '$2a$10$/iOt2Bx.vd4PHhWWJttE3.mNlX/jriVf4dASqOwbijXR.0goXmyRe', 'RECEP', 'http://localhost:8080/images/default.png');
